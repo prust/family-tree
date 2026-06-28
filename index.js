63d7b017fe679f3ed86af58a086f2c60a1e6b13e
Peter Rust <peter@cornerstonenw.com>
2026-06-28 08:48:49 -0700

    Added short-note

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 let tree = { name: 'Philip Samuel Rust', b: 1976, loc: 'Bellingham', parents: [
-    { name: 'Patrick Nick Rust', b: 1951, loc: 'San Diego', notes: 'Carpenter.', parents: [
-      { name: 'James Nicholas Rust', b: 1928, notes: 'Postmaster. "Tonto" on Orcas because his brother-in-law, Cliff Vierick, is "The Lone Ranger"', parents: [
-        { name: 'Samuel Philip Rust', b: 1903, notes: 'Bellingham Fire Chief. Served in the Navy and worked for the US government in Turkey.', parents: [
+    { name: 'Patrick Nick Rust', b: 1951, loc: 'San Diego', short_notes: 'Carpenter', parents: [
+      { name: 'James Nicholas Rust', b: 1928, short_notes: 'Postmaster', notes: '"Tonto" on Orcas because his brother-in-law, Cliff Vierick, is "The Lone Ranger"', parents: [
+        { name: 'Samuel Philip Rust', b: 1903, short_notes: 'Bellingham Fire Chief', notes: 'Served in the Navy and worked for the US government in Turkey.', parents: [
           { name: 'Nicholas J. Rust', b: 1873, d: 1967, notes: 'Bellingham Police Chief. Lost tips of fingers in sawmill. We have his billy club. Was stopped after running a stop light on Holly St. When asked if he hadn\'t seen the light, answered: "Sonny, I PUT that light there!"', parents: [
-            { name: 'Philip Rust', notes: 'Probably German origin' },
+            { name: 'Philip Rust', short_notes: 'Probably German origin' },
             { name: 'Ellen' }
           ]},
           { name: 'Maud Humphries', parents: [
-            { name: 'Samuel Humphries', notes: 'Original settlers on Lake Samish.' },
+            { name: 'Samuel Humphries', short_notes: 'Lake Samish settlers', notes: 'Original settlers on Lake Samish.' },
             { name: 'Mary Hemberger' }
           ]}
         ]},
@@ -17,11 +17,11 @@ let tree = { name: 'Philip Samuel Rust', b: 1976, loc: 'Bellingham', parents: [
         ]}
       ]},
       { name: 'Camilla Ernestine Rankin', b: 1930, loc: 'Tennessee', notes: 'Cami and Jim Rust were married at Pearl Harbor, Hawaii', parents: [
-        { name: 'Ernest Leonard Rankin', b: 1906, notes: 'Coal miner.', parents: [
-          { name: 'Rankin', notes: 'Coal miner.' },
+        { name: 'Ernest Leonard Rankin', b: 1906, short_notes: 'Coal miner.', parents: [
+          { name: 'Rankin', short_notes: 'Coal miner.' },
           { name: 'McCoy', parents: [
             {},
-            { notes: 'Cherokee Indian'}
+            { short_notes: 'Cherokee Indian'}
           ]}
         ]},
         {
@@ -50,36 +50,36 @@ let tree = { name: 'Philip Samuel Rust', b: 1976, loc: 'Bellingham', parents: [
       ]}
     ]},
     { name: 'Jean Marie MacLeod', b: 1952, loc: 'Bellingham', parents: [
-      { name: 'Duncan MacLeod', b: 1924, loc: 'Lopez', notes: 'Xerox tech. rep. Hobby: Flying. Dropped out of high school for WWII and finished later.', parents: [
-        { name: 'Malcolm James MacLeod', b: 1890, loc: 'Scotland', d: 1983, notes: 'Lied about his age to get into the service for WWI. Dairy farmer. His sister, Mary, is married to Fred Trump (real estate empire in NYC).', parents: [
+      { name: 'Duncan MacLeod', b: 1924, loc: 'Lopez', short_notes: 'Xerox tech. rep.', notes: 'Hobby: Flying. Dropped out of high school for WWII and finished later.', parents: [
+        { name: 'Malcolm James MacLeod', b: 1890, loc: 'Scotland', d: 1983, short_notes: 'Dairy farmer', notes: 'Lied about his age to get into the service for WWI. His sister, Mary, is married to Fred Trump (real estate empire in NYC).', parents: [
           { name: 'MacLeod', notes: 'Descended from 13th century king, Olsoc the Black King Leod of Denmark. Had two sons, one became MacLeod of Skye (Scotland), the other of Isle of Lewis (Scotland), which we are descended from.' },
           { name: 'Smith', parents: [
-            { name: 'Smith', notes: 'Fisherman, drowned.' },
+            { name: 'Smith', short_notes: 'Fisherman, drowned' },
             {}
           ]}
         ]},
         { name: 'Mary (Mae) Emma Higgins', b: 1893, loc: 'Vancouver, BC', notes: 'Aunt Kate came to the US in 1905.', parents: [
           { name: 'James Higgins', loc: 'Wales', parents: [
-            { name: 'Higgins', notes: 'Moved from Ireland to Wales. Railroad family.' },
+            { name: 'Higgins', short_notes: 'Railroad family', notes: 'Moved from Ireland to Wales.' },
             {}
           ]},
           { loc: 'Wales' }
         ]}
       ]},
       { name: 'Beverly Jean Hyland', b: 1930, loc: 'N. Dakota', parents: [
-        { name: 'William Clinton Hyland', b: 1887, d: 1971, notes: 'Farmer until 36', parents: [
+        { name: 'William Clinton Hyland', b: 1887, d: 1971, short_notes: 'Farmer until 36', parents: [
           { name: 'Hyland', loc: 'Illinois' },
           {}
         ]},
-        { name: 'Ethel Irene Dunwoodie', b: 1894, loc: 'N. Dakota', d: 1985, notes: '29 great-grandchildren.', parents: [
-          { name: 'Jesse Joseph Dunwoodie', notes: 'Pennysylvania Dutch', parents: [
+        { name: 'Ethel Irene Dunwoodie', b: 1894, loc: 'N. Dakota', d: 1985, short_notes: '29 great-grandchildren.', parents: [
+          { name: 'Jesse Joseph Dunwoodie', short_notes: 'Pennysylvania Dutch', parents: [
             { name: '...to Thomas Jefferson' },
             {}
           ]},
           { name: 'Mary Rines', notes: 'Scottish/Irish', parents: [
             { name: 'Rines', loc: 'Indiana', parents: [
               {},
-              { name: 'Barnard', loc: 'North Carolina', notes: 'Fought in Civil War.'}
+              { name: 'Barnard', loc: 'North Carolina', short_notes: 'Fought in Civil War.'}
             ]},
             {}
           ]}
@@ -118,6 +118,7 @@ $('#tree').innerHTML += `<text id="psn_${person.ix}" font-family="Arial" font-si
   let bbox = $('#psn_' + person.ix).getBBox();
 
   let loc_bbox = { width: 0, height: 0 };
+  let note_bbox = { width: 0, height: 0 };
   if (person.b || person.loc) {
     let info = [];
     if (person.b) {
@@ -131,10 +132,16 @@ $('#tree').innerHTML += `<text id="psn_${person.ix}" font-family="Arial" font-si
     </text>`;
     loc_bbox = $('#loc_' + person.ix).getBBox();
   }
-  person.my_width = Math.max(bbox.width, loc_bbox.width);
+  if (person.short_notes) {
+    $('#tree').innerHTML += `<text id="note_${person.ix}" font-family="Arial" font-style="italic" font-size="12" fill="#999">
+      ${person.short_notes}
+    </text>`;
+    note_bbox = $('#note_' + person.ix).getBBox();
+  }
+  person.my_width = Math.max(bbox.width, loc_bbox.width, note_bbox.width);
   person.my_height = bbox.height;// + loc_bbox.height;
   width += person.my_width;
-  height = Math.max(height, bbox.height + loc_bbox.height);
+  height = Math.max(height, bbox.height + loc_bbox.height + note_bbox.height);
   //height += person.my_height;
   person.width = width;
   person.height = height;
@@ -150,14 +157,25 @@ function layoutPerson(person, x, y) {
   person.y = y + person.height / 2;
   person.x = x;
 
+  let my_y = person.y + person.my_height * 1/3;
   let el = $('#psn_' + person.ix);
   el.setAttribute('x', person.x);
-  el.setAttribute('y', person.y + person.my_height * 1/3);
+  el.setAttribute('y', my_y);
+  if (person.name) {
+    my_y += person.my_height;
+  }
 
-  if (person.b || person.loc) {
+  let has_second_line = person.b || person.loc;
+  if (has_second_line) {
     let el = $('#loc_' + person.ix);
     el.setAttribute('x', person.x);
-    el.setAttribute('y', person.y + person.my_height * 1/3 + person.my_height);
+    el.setAttribute('y', my_y);
+    my_y += person.my_height;
+  }
+  if (person.short_notes) {
+    let el = $('#note_' + person.ix);
+    el.setAttribute('x', person.x);
+    el.setAttribute('y', my_y);
   }
 
   if (person.parents) {
